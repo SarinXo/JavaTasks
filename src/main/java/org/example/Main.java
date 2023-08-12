f86c722c0217efdb79fc2c1eb414d8f54df2af3f
rollo <rollovitch.roll@yandex.ru>
2023-08-12 16:50:12 +0300

    t3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,32 @@
 package org.example;
 
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args)  {
-        Scanner in = new Scanner(System.in);
+        int[] randValues = new int[20];
 
-        out.println("Введите a, b, c");
-        int a, b, c;
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
+        // диапазон [1; 15] - включительно
+        randValues = Arrays.stream(randValues)
+                .map(num ->(1 + (int)(Math.random() * 15) ))
+                .toArray();
 
-        {
-            int divider = 5;
+        Arrays.stream(randValues)
+                .forEach(System.out::println);
 
-            boolean aBy5 = printIfDivisibleBy('a', a, divider);
-            boolean bBy5 = printIfDivisibleBy('b', b, divider);
-            boolean cBy5 = printIfDivisibleBy('c', c, divider);
+        Map<Integer, Long> map = Arrays.stream(randValues)
+                .boxed()
+                .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
 
-            if (!aBy5 && !bBy5 && !cBy5) {
-                out.println("нет значений, кратных 5");
-            }
-        }
-
-        out.printf("Значение от целочисленного деления a на b (результат - целое число) a/b=%d\n", a/b);
-
-        out.printf("Значение от деления a на b (результат - число с плавающей запятой) a/b=%f\n", (double)a/b);
-
-        out.printf("Значение от деления a на b (результат - число с плавающей запятой), " +
-                "округленного до ближайшего целого в БОЛЬШУЮ сторону a/b=%d\n", (long)Math.ceil((double)a/b));
-
-        out.printf("Значение от деления a на b (результат - число с плавающей запятой), " +
-                "округленного до ближайшего целого в МЕНЬШУЮ сторону a/b=%d\n", (long)Math.floor((double)a/b));
-
-        out.printf("Значение от деления a на b (результат - число с плавающей запятой), " +
-                "округленного до ближайшего целого в МАТ округлением a/b=%d\n", Math.round((double)a/b));
-
-        out.printf("Остаток от деления b на c b%%c=%d\n", b%c);
-
-        out.printf("Наименьшее значение из a и b =%d\n", Math.min(a, b));
-
-        out.printf("Наибольшее значение из b и c =%d\n", Math.max(b, c));
+        map.entrySet().stream()
+                .filter(cell -> cell.getValue() > 1)
+                .forEach(cell -> System.out.printf("число '%d' встречается %d раза\n", cell.getKey(), cell.getValue()));
     }
 
-    private static boolean printIfDivisibleBy(char letter, int divisible, int divider){
-        if(divisible % divider == 0){
-            out.printf(letter + "=%d\n", divisible);
-            return true;
-        }
-        return false;
-    }
 }
