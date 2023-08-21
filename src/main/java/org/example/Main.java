30239b6dc2f33b779b90445c1b27078f564b6063
rollo <rollovitch.roll@yandex.ru>
2023-08-21 12:24:15 +0300

    4.4 Task 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,6 @@ package org.example;
 import org.example.educate.Learner;
 import org.example.educate.Student;
 
-import java.util.Scanner;
-
-import static java.lang.System.out;
-
 public class Main {
     public static void main(String[] args)  {
         Learner student = new Student();
